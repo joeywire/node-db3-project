91bed2f6b84e7c12369b3d29d90414d35f9ca046
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-09 16:32:58 -0600

    Crusin on these enpoints

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -4,6 +4,25 @@ const db = require('../../data/db-config');
 module.exports = {
   find() {
     return db('schemes');
+  },
+  findById(id) {
+    const returnObj = db('schemes').where('id', id).first();
+    if (!returnObj) {
+      return null;
+    } else return returnObj;
+  },
+  findSteps(id) {
+    //returns array of all correctly orerd step of the given scheme 
+    //array should include scheme_name not the scheme_id 
+    return db('steps as st')
+      .join('schemes as sc', 'sc.id', 'st.scheme_id')
+      .select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+      .where('st.scheme_id', id)
+      .orderBy('st.id', 'asc');
+  }, 
+  add(scheme) {
+    return db('schemes')
+      .insert(scheme, 'id');
   }
-
 };
+
